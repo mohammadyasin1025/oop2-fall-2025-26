5178618de0fe2d9956b17019ed437bbe257fe52e
mohammadyasin1025 <mohammadyasin1025@gmail.com>
2025-11-03 14:59:19 +0600

    23-51400-1

---
+++ project/Calculator/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter the first number: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ("Enter the second number: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+do
+{
+
+
+
+    Console.WriteLine("Select a calculation method:");
+    Console.WriteLine("1.Addition (+)");
+    Console.WriteLine("2.Substraction (-)");
+    Console.WriteLine("3.Multiplication (*)");
+    Console.WriteLine("4. Division (/)");
+    Console.Write("Enter your choice:1-4");
+    
+}
