ed495dbc19425c8b4560de8e2ee6349dc5e837e1
mohammadyasin1025 <mohammadyasin1025@gmail.com>
2025-11-12 14:59:05 +0600

    23-51400-1

---
+++ lab/HouseExercise/HouseExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿public class House
+{
+    public int YearBuilt {  get; set; }
+    public double Size { get; set; }
+     
+    public House (int YearBuilt, double Size)
+    {   this.YearBuilt = YearBuilt; 
+        this.Size = Size;
+    }
+    public int HowOld()
+    { 
+        int currentYear = DateTime.Now.Year;
+        int x = currentYear - YearBuilt;
+        return x;
+    }
+    public bool CanBeSold()
+    {
+        int age = HowOld();
+        return age > 15;.
+    }
+}
+public class Program
+{
+    public static void Main ()
+    {
+
+    }
+}
