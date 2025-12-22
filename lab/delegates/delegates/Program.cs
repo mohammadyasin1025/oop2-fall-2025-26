32c2b3c2dfe3667040ddfcb7365024d4dc07541d
mohammadyasin1025 <mohammadyasin1025@gmail.com>
2025-12-22 22:27:31 +0600

    23-51400-1

---
+++ lab/delegates/delegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DelegateDemo
+{
+    public delegate int Operation(int a, int b);
+
+    class Program
+    {
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        static void Calculate(int a, int b, Operation op)
+        {
+            int result = op(a, b);
+            Console.WriteLine("Result: " + result);
+        }
+
+        static void Main(string[] args)
+        {
+            Calculate(10, 5, Add);
+            Calculate(10, 5, Subtract);
+        }
+    }
+}
