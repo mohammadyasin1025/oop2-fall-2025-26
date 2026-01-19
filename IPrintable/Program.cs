22be2bf9c17556cc647c3f1fd42a1bf2d4633527
mohammadyasin1025 <mohammadyasin1025@gmail.com>
2026-01-20 03:10:11 +0600

    23-51400-1

---
+++ IPrintable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+interface IPrintable
+{
+    void Print();
+}
+interface ISerializable
+{
+    void Save();
+}
+class Report : IPrintable, ISerializable
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing the report");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Saving the report");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Report r = new Report();
+        r.Print();
+        r.Save();
+    }
+}
