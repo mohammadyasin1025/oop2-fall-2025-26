5e29d1c90291ad8d3d14d060b66de4503b0cc927
mohammadyasin1025 <mohammadyasin1025@gmail.com>
2026-01-20 02:50:23 +0600

    23-51400-1

---
+++ Employee/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+abstract class Employee
+{
+    public double basicSalary;
+
+    public abstract double GetTotalSalary();
+}
+
+class Manager : Employee
+{
+    public override double GetTotalSalary()
+    {
+        return basicSalary + 2000;
+    }
+}
+
+class Worker : Employee
+{
+    public override double GetTotalSalary()
+    {
+        return basicSalary + 800;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager m = new Manager();
+        m.basicSalary = 10000;
+
+        Worker w = new Worker();
+        w.basicSalary = 8000;
+
+        Console.WriteLine("Manager Salary: " + m.GetTotalSalary());
+        Console.WriteLine("Worker Salary: " + w.GetTotalSalary());
+    }
+}
